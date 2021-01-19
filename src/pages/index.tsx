f9e89900e4c78ea67d8ce19fb2c49d480d9aee1c
Shubh <shubhamratrey@gmail.com>
2021-01-19 22:58:37 +0530

    changes in home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Hi, I'm Shubh
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <span className="handle">@shubhamratrey</span>
+          <h2>Meet Me In Montauk </h2>
           <SocialList />
         </div>
       </div>
